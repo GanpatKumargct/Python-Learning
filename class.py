1e56263ada23eca0f1ba765d8e185c2770a9689a
Ganpat Kumar <ganpatstudy@gmail.com>
2026-01-06 08:36:58 +0530

    class under user defined function

--- class.py
+++ class.py
@@ -7,12 +7,18 @@ class car:
         self.model = model
         self.brand = brand
 
+    def full_name(self):
+        return f"{self.model} {self.brand}"
 
 
+# Object
 c = car("2025","Thar")
 print(c.brand)
 print(c.model)
+print(c.full_name())
 
 c2 = car("maruti","suzki")
 print(c2.brand)
 print(c2.model)
+print(c2.full_name())
+
