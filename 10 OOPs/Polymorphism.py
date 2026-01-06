772b2fd3dfded3748c2d2c40235314b8ca55e790
Ganpat Kumar <ganpatstudy@gmail.com>
2026-01-06 11:17:21 +0530

    polymorphism - method overriding

---
+++ 10 OOPs/Polymorphism.py
@@ -0,0 +1,34 @@
+
+class Car:
+
+    # Constructor --  and self = this
+    def __init__(self, model, brand):
+        self.__model = model       # __ under score ka matalb hota hai yaha ki attributes is private
+        self.__brand = brand
+
+    def full_name(self):
+        return f"{self.__model} {self.__brand}"
+    
+    def get_model(self):
+        return self.__model + " !"
+
+
+    def get_brand(self):
+        return f"{self.__brand} this is a private brand"
+    
+    def fuel_type(self):
+        return "disel or petrol"
+
+class ElectricCar(Car):
+    def __init__(self, model, brand, battery_size):
+        super().__init__(model, brand)
+        self.battery_size = battery_size
+
+    def fuel_type(self):
+        return "Battery charge"
+    
+safari = Car("Safari","Tata")
+print(safari.fuel_type())
+
+suv = ElectricCar("SUV", "Tata", "70kwh")
+print(suv.fuel_type())
